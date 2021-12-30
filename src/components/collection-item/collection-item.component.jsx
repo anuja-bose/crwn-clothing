aee31056edaa32b0aa81b29dbc5fc073088b90d8
Anuja Bose <anuja.joydeep.bose@gmail.com>
2021-12-30 14:50:24 +0530

    test

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import './collection-item.styles.scss';
-
+//test
 const CollectionItem = ({ id, name, price, imageUrl }) => (
   <div className='collection-item'>
     <div
