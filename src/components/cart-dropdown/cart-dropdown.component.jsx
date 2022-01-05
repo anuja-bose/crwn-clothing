8668bb735a23ef503025a7c3f8479cddb1edb950
Anuja Bose <anuja.joydeep.bose@gmail.com>
2022-01-05 10:42:56 +0530

    added total price to the redux selector, and add elements to the checkout page

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
@@ -9,7 +10,7 @@ import CartItem from '../cart-item/cart-item.component';
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = ({cartItems}) => (
+const CartDropdown = ({cartItems, history}) => (
   <div className='cart-dropdown'>
     <div className='cart-items' >
     { cartItems.length ? ( cartItems.map(cartItem => (<CartItem key={cartItem.id} item={cartItem} />))) 
@@ -18,7 +19,7 @@ const CartDropdown = ({cartItems}) => (
     }
     </div>
 
-    <CustomButton>GO TO CHECKOUT</CustomButton>
+    <CustomButton onClick = {()=> history.push('/checkout')}>GO TO CHECKOUT</CustomButton>
   </div>
 );
 
@@ -27,4 +28,4 @@ const CartDropdown = ({cartItems}) => (
 const mapStateToProps =  createStructuredSelector({
   cartItems : selectCartItems
 })
-export default connect(mapStateToProps)(CartDropdown);
+export default withRouter(connect(mapStateToProps)(CartDropdown));
