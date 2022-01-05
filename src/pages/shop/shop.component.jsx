01572da879104ef3160ecc5af7dde293137b2f62
Anuja Bose <anuja.joydeep.bose@gmail.com>
2022-01-05 17:04:45 +0530

    collection overview

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-import { selectCollections} from '../../redux/shop/shop.selectors'
 
-import CollectionPreview from '../../components/collection-preview/collection-preview';
+import CollectionsOverview from '../../components/collections-overview/collection-overview.component';
 
-const ShopPage = ({collections})=> {
- console.log("collections",collections);
-  return  ( 
-    <div className='shop-page'>
-      {collections.map(({ id, ...otherCollectionProps }) => (
-        <CollectionPreview key={id} {...otherCollectionProps} />
-      ))}
-    </div>
-  );
-}
- 
+const ShopPage = () => (
+  <div className='shop-page'>
+    <CollectionsOverview />
+  </div>
+)
 
-const mapStateToProps =  createStructuredSelector({
-  collections : selectCollections
-});
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
