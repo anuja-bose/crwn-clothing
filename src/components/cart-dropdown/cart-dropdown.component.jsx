6e1cdb30b7488c26b55b31493e310c4a9e2509e0
Anuja Bose <anuja.joydeep.bose@gmail.com>
2022-01-05 11:08:23 +0530

    close the popup when checkout button is clicked

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,23 +3,26 @@ import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import { selectCartItems } from '../../redux/cart/cart.selectors';
-
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
 
+import { selectCartItems } from '../../redux/cart/cart.selectors';
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
+
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = ({cartItems, history}) => (
+const CartDropdown = ({cartItems, history, dispatch}) => (
   <div className='cart-dropdown'>
     <div className='cart-items' >
     { cartItems.length ? ( cartItems.map(cartItem => (<CartItem key={cartItem.id} item={cartItem} />))) 
     : ( <span className='empty-message'> Your cart is empty </span>)
-     
     }
     </div>
 
-    <CustomButton onClick = {()=> history.push('/checkout')}>GO TO CHECKOUT</CustomButton>
+    <CustomButton onClick = {
+      ()=> {history.push('/checkout');
+      dispatch(toggleCartHidden());}
+      }> GO TO CHECKOUT</CustomButton>
   </div>
 );
 
