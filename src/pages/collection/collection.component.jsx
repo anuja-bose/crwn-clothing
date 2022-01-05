75130e9cef27d6eb3fdea2fbbf6e888600e6a7a1
Anuja Bose <anuja.joydeep.bose@gmail.com>
2022-01-05 19:18:07 +0530

    renamed category to collection

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './collection.styles.scss';
+
+const CollectionPage = ({match}) => {
+    console.log("match",match); 
+    return (<div className='collection'>
+        <h2>Collection PAGE</h2>
+        </div>)
+    }
+
+
+export default CollectionPage;
