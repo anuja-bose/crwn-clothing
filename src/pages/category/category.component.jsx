60004185ba8a5f552de9da3e1f9ba1331e8706c6
Anuja Bose <anuja.joydeep.bose@gmail.com>
2022-01-05 18:46:47 +0530

    nesting route added to shop component

---
+++ src/pages/category/category.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './category.styles.scss';
+
+const CategoryPage = ({match}) => {
+    console.log("match",match); 
+    return (<div className='category'>
+        <h2>CATEGORY PAGE</h2>
+        </div>)
+    }
+
+
+export default CategoryPage;
